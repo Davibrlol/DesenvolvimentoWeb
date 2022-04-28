fb610b184a1ab595a0e8270e2fc7dece46147b0d
Davi <daviescolaifc@gmail.com>
2022-04-28 10:37:29 -0300

    Atividade DW2

---
+++ Atividade-DW2/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhoPrincipal from "./components/CabecalhoPrincipal.vue";
+import MenuPrincipal from "./components/MenuPrincipal.vue";
+import RodapePrincipal from "./components/RodapePrincipal.vue";
+export default {
+  components: { MenuLateral, CabecalhoPrincipal, MenuPrincipal, RodapePrincipal },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabecalhoPrincipal />
+  <MenuPrincipal />
+  <RodapePrincipal />
+</template>
+<style></style>
